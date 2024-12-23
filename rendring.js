e89f2b083c8a43f0e5e1d5c14c42698785cfec3f
echchabli hamza <echchablihamza1@gmail.com>
2024-12-23 20:05:33 +0100

    some components

---
+++ rendring.js
@@ -0,0 +1,95 @@
+
+
+
+
+const board = [
+    [-2,-3,-4,-5,-6,-4,-3,-2],
+    [-1,-1,-1,-1,-1, -1,-1,-1],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1],
+    [2, 3, 4, 5, 6, 4, 3, 2]
+  ];
+
+
+
+
+  const pieceMap = {
+    '1': 'wp',  
+    '2': 'wr',  
+    '3': 'wn',  
+    '4': 'wb',  
+    '5': 'wq',  
+    '6': 'wk',
+    '-1': 'bp', 
+    '-2': 'br', 
+    '-3': 'bn', 
+    '-4': 'bb', 
+    '-5': 'bq', 
+    '-6': 'bk' 
+
+};
+  
+function renderBoard() {
+    const boardElement = document.querySelector('.board');
+    boardElement.innerHTML = '';
+
+    boardElement.innerHTML =    '<svg class="numLett" viewBox="0 0 100 100" class="coordinates"><text x="0.75" y="3.5" font-size="2.8" class="coordinate-light">8</text><text x="0.75" y="15.75" font-size="2.8" class="coordinate-dark">7</text><text x="0.75" y="28.25" font-size="2.8" class="coordinate-light">6</text><text x="0.75" y="40.75" font-size="2.8" class="coordinate-dark">5</text><text x="0.75" y="53.25" font-size="2.8" class="coordinate-light">4</text><text x="0.75" y="65.75" font-size="2.8" class="coordinate-dark">3</text><text x="0.75" y="78.25" font-size="2.8" class="coordinate-light">2</text><text x="0.75" y="90.75" font-size="2.8" class="coordinate-dark">1</text><text x="10" y="99" font-size="2.8" class="coordinate-dark">a</text><text x="22.5" y="99" font-size="2.8" class="coordinate-light">b</text><text x="35" y="99" font-size="2.8" class="coordinate-dark">c</text><text x="47.5" y="99" font-size="2.8" class="coordinate-light">d</text><text x="60" y="99" font-size="2.8" class="coordinate-dark">e</text><text x="72.5" y="99" font-size="2.8" class="coordinate-light">f</text><text x="85" y="99" font-size="2.8" class="coordinate-dark">g</text><text x="97.5" y="99" font-size="2.8" class="coordinate-light">h</text></svg>' 
+
+
+    for (let row = 0; row < 8; row++) {
+        for (let col = 0; col < 8; col++) {
+            const sq = document.createElement('div');
+            sq.classList.add('pieceContainer'),
+         
+            sq.style.gridColumn = col + 1;
+            sq.style.gridRow = row + 1;
+
+            sq.dataset.row = row;
+            sq.dataset.col = col;
+            sq.dataset.content = 0;
+
+            const pieceNum = board[row][col];
+            if (pieceNum !== 0) {
+                // sq.dataset.code=pieceNum;
+                const piece = pieceMap[pieceNum]; 
+                sq.dataset.content = pieceNum;
+                const img = document.createElement('img');
+                img.src = `pieces/${piece}.png`; 
+                img.alt = piece;
+                img.classList.add('piece');
+                sq.appendChild(img);
+                 
+            }
+           
+
+           boardElement.appendChild(sq);
+        }
+    }
+    AddEvents();
+}
+
+renderBoard();
+
+document.getElementById('clc').addEventListener('click' ,function test() {
+
+  console.log('rr');
+  
+    
+    
+} )
+
+
+function AddEvents() {
+    const allPieces = document.querySelectorAll('.pieceContainer');
+
+    Array.from(allPieces).forEach(element => {
+        element.addEventListener('click', function () {
+            console.log('Clicked on:', element);
+        });
+    });
+}
+
+
